f8832f20a595329876fbd49aa063310c3325bb5f
Sebastian Wiegandt <sebastian.wiegandt@gmail.com>
2023-04-03 03:44:38 +0200

    Refactor loops

--- 2022/src/days/day_12.rs
+++ 2022/src/days/day_12.rs
@@ -37,40 +37,36 @@ fn find_path<F: Fn(char) -> bool>(input: &Map<char>, start: F) -> i32 {
     let y_len = distances.len();
 
     'outer: loop {
-        distance += 1;
         let mut next_nodes = HashSet::new();
+        distance += 1;
 
-        for (y, x) in current_nodes.iter() {
-            if input[*y][*x] == 'E' {
+        for (y, x) in current_nodes {
+            if input[y][x] == 'E' {
                 break 'outer distance - 1;
             }
 
-            let elevation = to_elevation(input[*y][*x]);
-            let x_len = distances[*y].len();
+            let elevation = to_elevation(input[y][x]);
+            let x_len = distances[y].len();
 
-            for y_neighbor in [y.checked_sub(1), y.checked_add(1)]
+            [y.checked_sub(1), y.checked_add(1)]
                 .iter()
                 .filter_map(|&y| y.filter(|&y| y < y_len))
-            {
-                if to_elevation(input[y_neighbor][*x]) <= elevation + 1 {
-                    if distances[y_neighbor][*x].is_none() {
-                        next_nodes.insert((y_neighbor, *x));
-                        distances[y_neighbor][*x] = Some(distance);
+                .for_each(|y| {
+                    if to_elevation(input[y][x]) <= elevation + 1 && distances[y][x].is_none() {
+                        next_nodes.insert((y, x));
+                        distances[y][x] = Some(distance);
                     }
-                }
-            }
+                });
 
-            for x_neighbor in [x.checked_sub(1), x.checked_add(1)]
+            [x.checked_sub(1), x.checked_add(1)]
                 .iter()
                 .filter_map(|&x| x.filter(|&x| x < x_len))
-            {
-                if to_elevation(input[*y][x_neighbor]) <= elevation + 1 {
-                    if distances[*y][x_neighbor].is_none() {
-                        next_nodes.insert((*y, x_neighbor));
-                        distances[*y][x_neighbor] = Some(distance);
+                .for_each(|x| {
+                    if to_elevation(input[y][x]) <= elevation + 1 && distances[y][x].is_none() {
+                        next_nodes.insert((y, x));
+                        distances[y][x] = Some(distance);
                     }
-                }
-            }
+                });
         }
 
         current_nodes = next_nodes.into_iter().collect::<Vec<_>>();
