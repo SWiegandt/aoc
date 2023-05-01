fcb5311f9b7a384c073c8073c95e4506203ba748
Sebastian Wiegandt <sebastian.wiegandt@gmail.com>
2023-05-01 18:50:32 +0200

    Solve day 10

---
+++ 2015/cmd/day10/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+
+	"github.com/SWiegandt/aoc/2015/pkg/util"
+)
+
+func lookAndSay(input string) string {
+	var prev rune
+	var count int64
+	var output []byte
+
+	for _, curr := range input {
+		if prev != 0 && curr != prev {
+			output = strconv.AppendInt(output, count, 10)
+			output = utf8.AppendRune(output, prev)
+			count = 0
+		}
+
+		count++
+		prev = curr
+	}
+
+	output = strconv.AppendInt(output, count, 10)
+	output = utf8.AppendRune(output, prev)
+
+	return string(output)
+}
+
+func iterate(input string, count int) string {
+	for i := 0; i < count; i++ {
+		input = lookAndSay(input)
+	}
+
+	return input
+}
+
+func ProblemOne(input string) int {
+	return len(iterate(input, 40))
+}
+
+func ProblemTwo(input string) int {
+	return len(iterate(input, 40))
+}
+
+func main() {
+	input := util.ReadInput(10)
+
+	fmt.Printf("Problem 1: %d\n", ProblemOne(input))
+	fmt.Printf("Problem 2: %d\n", ProblemTwo(input))
+}
