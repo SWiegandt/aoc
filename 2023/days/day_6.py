35d4b2fc76cafa5d2b3dbdfb34f0581cbd3d9ea9
Sebastian Wiegandt <sebastian.wiegandt@gmail.com>
2023-12-07 02:52:07 +0100

    day 6 refactor

--- 2023/days/day_6.py
+++ 2023/days/day_6.py
@@ -8,29 +8,24 @@ from day import Day
 class DaySix(Day):
     day = 6
 
-    def one(self, input):
-        times = map(int, re.split(r"\s+", input[0])[1:])
-        distances = map(int, re.split(r"\s+", input[1])[1:])
-        product = 1
-
-        for time, distance in zip(times, distances):
-            product *= (
-                math.ceil((time / 2) + math.sqrt((time / 2) ** 2 - distance) - 1)
-                - math.floor((time / 2) - math.sqrt((time / 2) ** 2 - distance) + 1)
-                + 1
-            )
-
-        return product
-
-    def two(self, input: List[str]):
-        time = int(input[0].split(" ", maxsplit=1)[1].replace(" ", ""))
-        distance = int(input[1].split(" ", maxsplit=1)[1].replace(" ", ""))
-
+    def record_beaters(self, time, distance):
         return (
             math.ceil((time / 2) + math.sqrt((time / 2) ** 2 - distance) - 1)
             - math.floor((time / 2) - math.sqrt((time / 2) ** 2 - distance) + 1)
             + 1
         )
+
+    def one(self, input):
+        times = map(int, re.split(r"\s+", input[0])[1:])
+        distances = map(int, re.split(r"\s+", input[1])[1:])
+
+        return math.prod(map(self.record_beaters, times, distances))
+
+    def two(self, input: List[str]):
+        time = int(input[0].split(" ", maxsplit=1)[1].replace(" ", ""))
+        distance = int(input[1].split(" ", maxsplit=1)[1].replace(" ", ""))
+
+        return self.record_beaters(time, distance)
 
 
 if __name__ == "__main__":
